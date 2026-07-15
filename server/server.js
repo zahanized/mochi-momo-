8f6408d8c5e7a98a43e7e0be0c5abe7b5ad8ce04
Niha Zahan <nishat.zahan.niha@g.bracu.ac.bd>
2026-07-15 17:14:09 +0600

    Step 3: Express server connected to MongoDB Atlas

---
+++ server/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
